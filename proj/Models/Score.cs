77c2dde7d0d5104faa084dcd9e8ed9f27404018a
eberilia <eberiliaa@gmail.com>
2020-05-22 17:04:09 +0200

    utworzona klasa score

---
+++ proj/Models/Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proj.Models
+{
+    public class Score
+    {
+        uint points;
+
+        public Score()
+        {
+            points = 0;
+        }
+
+        public uint getPoints()
+        {
+            return points;
+        }
+
+        public void PointPlusPlus()
+        {
+            points++;
+        }
+
+        
+
+    }
+}
