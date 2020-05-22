abb7be029d97c9b5e77c20f36406cf5e761d35be
eberilia <eberiliaa@gmail.com>
2020-05-22 14:53:06 +0200

    pierwsza proba czesc Oli

--- proj/Controllers/HomeController.cs
+++ proj/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace proj.Controllers
             return View();
         }
 
+        public IActionResult Stats()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
