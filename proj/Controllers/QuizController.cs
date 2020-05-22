103df1b14da77b223ce1c0c893f969f4ab2e0949
eberilia <eberiliaa@gmail.com>
2020-05-22 15:49:27 +0200

    kasia fix

---
+++ proj/Controllers/QuizController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proj.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
